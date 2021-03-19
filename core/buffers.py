fixed lower-bound loss calculation

core/buffers.py
@@ -591,16 +591,26 @@ class ValueReplayBuffer(BaseBuffer):
         self.actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=action_space.dtype)
         self.rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
         self.dones = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+
         self.optimal_values = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
         self.subopt_values = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
-        self.nstep_reward = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+
         self.nth_observations = np.zeros((self.buffer_size, self.n_envs) + self.obs_shape, dtype=observation_space.dtype)
         self.nth_actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=action_space.dtype)
+        self.nstep_reward = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
         self.nth_dones = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+
+        self.prev_nth_observations = np.zeros((self.buffer_size, self.n_envs) + self.obs_shape, dtype=observation_space.dtype)
+        self.prev_nth_actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=action_space.dtype)
+        self.prev_nstep_reward = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+
         self.nstep = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
 
         if psutil is not None:
-            total_memory_usage = self.observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes
+            total_memory_usage = self.observations.nbytes + self.actions.nbytes + self.rewards.nbytes + self.dones.nbytes + self.optimal_values.nbytes \
+                                + self.subopt_values.nbytes + self.nstep_reward.nbytes + self.nth_observations.nbytes + self.nth_actions.nbytes + self.nth_dones.nbytes \
+                                + self.prev_nstep_reward.nbytes + self.prev_nth_observations.nbytes + self.prev_nth_actions.nbytes + self.nstep.nbytes
+
             if self.next_observations is not None:
                 total_memory_usage += self.next_observations.nbytes
 
@@ -620,6 +630,9 @@ class ValueReplayBuffer(BaseBuffer):
             nth_observations: np.array,
             nth_actions: np.array,
             nth_done: np.ndarray,
+            prev_nth_observations: np.array,
+            prev_nth_actions: np.array,
+            prev_nstep_reward: np.array,
             nstep:np.array) -> None:
         # Copy to avoid modification by reference
         self.observations[self.pos] = np.array(obs).copy()
@@ -637,6 +650,9 @@ class ValueReplayBuffer(BaseBuffer):
         self.nth_observations[self.pos] = np.array(nth_observations).copy()
         self.nth_actions[self.pos] = np.array(nth_actions).copy()
         self.nth_dones[self.pos] = np.array(nth_done).copy()
+        self.prev_nth_observations[self.pos] = np.array(prev_nth_observations).copy()
+        self.prev_nth_actions[self.pos] = np.array(prev_nth_actions).copy()
+        self.prev_nstep_reward[self.pos] = np.array(prev_nstep_reward).copy()
         self.nstep[self.pos] = np.array(nstep).copy()
 
 
@@ -679,12 +695,18 @@ class ValueReplayBuffer(BaseBuffer):
             next_obs,
             self.dones[batch_inds],
             self._normalize_reward(self.rewards[batch_inds], env),
+
             self.optimal_values[batch_inds],
             self.subopt_values[batch_inds],
             self.nstep_reward[batch_inds],
+
             self._normalize_obs(self.nth_observations[batch_inds, 0, :], env),
             self.nth_actions[batch_inds, 0, :],
             self.nth_dones[batch_inds],
+            
+            self._normalize_obs(self.prev_nth_observations[batch_inds, 0, :], env),
+            self.prev_nth_actions[batch_inds, 0, :],
+            self.prev_nstep_reward[batch_inds],
             self.nstep[batch_inds]
         )
         return ValueReplayBufferSamples(*tuple(map(self.to_torch, data)))
@@ -697,12 +719,113 @@ class ValueReplayBufferSamples(NamedTuple):
     next_observations:th.Tensor
     dones:th.Tensor
     rewards:th.Tensor
+
+    optimal_values:th.Tensor
     subopt_values:th.Tensor
     nstep_reward:th.Tensor
-    optimal_values:th.Tensor
+
     nth_observations:th.Tensor
     nth_actions:th.Tensor
     nth_dones:th.tensor
+
+    prev_nth_observations:th.Tensor
+    prev_nth_actions:th.Tensor
+    prev_nstep_reward:th.Tensor
     nstep_gamma:th.Tensor
 
 
+
+class StateActionReplayBuffer(BaseBuffer):
+    """
+    Replay buffer used in off-policy algorithms like SAC/TD3.
+
+    :param buffer_size: Max number of element in the buffer
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param device:
+    :param n_envs: Number of parallel environments
+    :param optimize_memory_usage: Enable a memory efficient variant
+        of the replay buffer which reduces by almost a factor two the memory used,
+        at a cost of more complexity.
+        See https://github.com/DLR-RM/stable-baselines3/issues/37#issuecomment-637501195
+        and https://github.com/DLR-RM/stable-baselines3/pull/28#issuecomment-637559274
+    """
+
+    def __init__(
+        self,
+        buffer_size: int,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        device: Union[th.device, str]="cpu",
+        n_envs: int = 1,
+        optimize_memory_usage: bool = False,
+    ):
+        super(StateActionReplayBuffer, self).__init__(buffer_size, observation_space, action_space, device, n_envs=n_envs)
+
+        assert n_envs == 1, "Replay buffer only support single environment for now"
+
+        # Check that the replay buffer can fit into the memory
+        if psutil is not None:
+            mem_available = psutil.virtual_memory().available
+
+        self.optimize_memory_usage = optimize_memory_usage
+        self.observations = np.zeros((self.buffer_size, self.n_envs) + self.obs_shape, dtype=observation_space.dtype)
+        self.actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=action_space.dtype)
+       
+        if psutil is not None:
+            total_memory_usage = self.observations.nbytes + self.actions.nbytes 
+
+            if total_memory_usage > mem_available:
+                # Convert to GB
+                total_memory_usage /= 1e9
+                mem_available /= 1e9
+                warnings.warn(
+                    "This system does not have apparently enough memory to store the complete "
+                    f"replay buffer {total_memory_usage:.2f}GB > {mem_available:.2f}GB"
+                )
+
+    def add(self, obs: np.ndarray, action: np.ndarray) -> None:
+        # Copy to avoid modification by reference
+        self.observations[self.pos] = np.array(obs).copy()
+        self.actions[self.pos] = np.array(action).copy()
+
+        self.pos += 1
+        if self.pos == self.buffer_size:
+            self.full = True
+            self.pos = 0
+
+    def sample(self, batch_size: int, env: Optional[VecNormalize] = None) -> ReplayBufferSamples:
+        """
+        Sample elements from the replay buffer.
+        Custom sampling when using memory efficient variant,
+        as we should not sample the element with index `self.pos`
+        See https://github.com/DLR-RM/stable-baselines3/pull/28#issuecomment-637559274
+
+        :param batch_size: Number of element to sample
+        :param env: associated gym VecEnv
+            to normalize the observations/rewards when sampling
+        :return:
+        """
+        if not self.optimize_memory_usage:
+            return super().sample(batch_size=batch_size, env=env)
+        # Do not sample the element with index `self.pos` as the transitions is invalid
+        # (we use only one array to store `obs` and `next_obs`)
+        if self.full:
+            batch_inds = (np.random.randint(1, self.buffer_size, size=batch_size) + self.pos) % self.buffer_size
+        else:
+            batch_inds = np.random.randint(0, self.pos, size=batch_size)
+        return self._get_samples(batch_inds, env=env)
+
+    def _get_samples(self, batch_inds: np.ndarray, env: Optional[VecNormalize] = None) -> ReplayBufferSamples:
+        
+        data = (
+            self._normalize_obs(self.observations[batch_inds, 0, :], env),
+            self.actions[batch_inds, 0, :]
+        )
+        return StateActionReplayBufferSamples(*tuple(map(self.to_torch, data)))
+
+
+from typing import NamedTuple
+class StateActionReplayBufferSamples(NamedTuple):
+    observations: th.Tensor
+    actions:th.Tensor

core/off_policy_algorithm.py
@@ -19,7 +19,7 @@ from stable_baselines3.common.type_aliases import GymEnv, Schedule, MaybeCallbac
 from stable_baselines3.common.utils import safe_mean
 from stable_baselines3.common.vec_env import VecEnv
 
-from core.buffers import IdealReplayBuffer, ValueReplayBuffer
+from core.buffers import IdealReplayBuffer, ValueReplayBuffer, StateActionReplayBuffer
 
 th.manual_seed(0)
 
@@ -176,6 +176,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         self.normal_window = 3
 
         self.expert_mean_reward = 300
+        self.intermediate_max_r = 3
 
     def _set_expert_mean_reward(self, expert_mean_reward) -> None:
         self.expert_mean_reward = expert_mean_reward * 0.6
@@ -208,6 +209,14 @@ class OffPolicyAlgorithm(BaseAlgorithm):
             optimize_memory_usage=self.optimize_memory_usage,
         )
         self.expert_replay_buffer = ValueReplayBuffer(
+            self.buffer_size,
+            self.observation_space,
+            self.action_space,
+            self.device,
+            optimize_memory_usage=self.optimize_memory_usage,
+        )
+
+        self.state_action_buffer = StateActionReplayBuffer(
             self.buffer_size,
             self.observation_space,
             self.action_space,
@@ -305,7 +314,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         )
 
         callback.on_training_start(locals(), globals())
-
+        #self.env.render()
         while self.num_timesteps < total_timesteps:
             print('collecting rollout for learning')
             if not self.use_acceleration:
@@ -340,7 +349,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
             if self.num_timesteps > 1000 and self.num_timesteps > self.learning_starts and update_model:
                 # If no `gradient_steps` is specified,
                 # do as many gradients steps as steps performed during the rollout
-                gradient_steps = 1  # self.gradient_steps if self.gradient_steps > 0 else rollout.episode_timesteps
+                gradient_steps = 30  # self.gradient_steps if self.gradient_steps > 0 else rollout.episode_timesteps
                 self.train(batch_size=self.batch_size, gradient_steps=gradient_steps,
                            update_actor=True, weight_factor=self.num_timesteps % 1000)
 
@@ -461,8 +470,9 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         fps = int(self.num_timesteps / (time.time() - self.start_time))
         logger.record("time/episodes", self._episode_num, exclude="tensorboard")
         if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
-            logger.record("rollout/ep_rew_mean", safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
-            logger.record("rollout/ep_len_mean", safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
+            #print([ep_info["r"] for ep_info in self.ep_info_buffer])
+            logger.record("rollout/ep_rew_mean", [ep_info["r"] for ep_info in self.ep_info_buffer][-1])#safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
+            logger.record("rollout/ep_len_mean", [ep_info["l"] for ep_info in self.ep_info_buffer][-1])#safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
         logger.record("time/fps", fps)
         logger.record("time/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
         logger.record("time/total timesteps", self.num_timesteps, exclude="tensorboard")
@@ -552,6 +562,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         while total_steps < n_steps or total_episodes < n_episodes:
             done = False
             episode_reward, episode_timesteps, original_episode_reward = 0.0, 0, 0.0
+
             self.rewarder.reset()
 
             while not done:
@@ -1172,6 +1183,8 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                 imitation_reward = self.rewarder.compute_reward(obs_act)
                 reward = imitation_reward
                 #env.render()
+                if self.intermediate_max_r < imitation_reward:
+                    self.intermediate_max_r = imitation_reward
 
                 self.num_timesteps += 1
                 episode_timesteps += 1
@@ -1228,6 +1241,7 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                                    1e3)
                 """
                 if in_expert:
+                    #print("in")
                     traj.append([self._last_original_obs,  new_obs_, buffer_action,
                                  imitation_reward, done, in_expert, tmp_sub_value, tmp_opt_value])
                 else:
@@ -1277,6 +1291,9 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                 # Log training infos
                 if log_interval is not None and self._episode_num % log_interval == 0:
                     self._dump_logs()
+                    if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
+                        logger.record("rollout/ep_imitation_mean", episode_reward)#safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
+            
                 print(episode_rewards, original_episode_reward, total_steps, total_episodes, continue_training)
 
         mean_reward = np.mean(episode_rewards) if total_episodes > 0 else 0.0
@@ -1303,7 +1320,13 @@ class OffPolicyAlgorithm(BaseAlgorithm):
         for idx in range(traj_len):
             trans = traj[idx]
             in_expert = trans[5]
-            if in_expert and (idx > (window-1)) and (idx < len(traj)-window):
+
+            prev_nth_discounted_R = 0
+            prev_id = np.max([idx - window, 0])
+            for istep, prev_sub_trans in enumerate(traj[prev_id:idx]):
+                prev_nth_discounted_R += self.gamma**(istep) * prev_sub_trans[3]
+
+            if False:#in_expert and (idx > (window-1)) and (idx < len(traj)-window):
                 # first add this expert transition
                 sub_Q = trans[-2]
                 opt_Q = trans[-1]
@@ -1344,6 +1367,9 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                            traj[idx+window][0], # state-action after window_step_th (lower-bound) 
                                            traj[idx+window][2], # state-action after window_step_th (lower-bound) 
                                            traj[idx+window][4], # check if after window_step_th is terminate state
+                                           traj[prev_id][0],
+                                           traj[prev_id][2],
+                                           prev_nth_discounted_R,
                                            self.gamma**window, # discounted gamma after window_step_th
                                            )
                         
@@ -1372,13 +1398,18 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                                    traj[idx+window][0], # state-action after pred_id -> window_step_th (lower-bound) 
                                                    traj[idx+window][2], # state-action after pred_id -> window_step_th (lower-bound) 
                                                    traj[idx+window][4], # check if after window_step_th is terminate state
+                                                   traj[prev_id][0],
+                                                   traj[prev_id][2],
+                                                   prev_nth_discounted_R,
                                                    self.gamma**(window+optimal_window), # discounted gamma after window_step_th
                                                    )
+
+                #self.state_action_buffer.add(trans[0],trans[2])
             else:
                 # first add this expert transition
                 sub_Q = trans[-2]
                 opt_Q = trans[-1]
-                tmp_end_idx = np.min((normal_window+idx, traj_len-1)) # in case tmp_window+idx > len(traj)
+                tmp_end_idx = np.min((window+idx, traj_len-1)) # in case tmp_window+idx > len(traj)
 
                 horizon_n = tmp_end_idx - idx
                 sub_traj = traj[idx : ]
@@ -1402,29 +1433,31 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                                    traj[idx+horizon_n][0],
                                                    traj[idx+horizon_n][2],
                                                    traj[idx+horizon_n][4],
+                                                   traj[prev_id][0],
+                                                   traj[prev_id][2],
+                                                   prev_nth_discounted_R,
                                                    self.gamma**horizon_n
                                                    )
 
-    def add_expert_trajs_to_buffer(self, parsed_trajs, value_dataset,  window=10,):  
-        window = self.forward_window#: int=10  
+    def add_expert_trajs_to_buffer(self, parsed_trajs, value_dataset,  max_window=10,):  
+        window = max_window#: int=10  
         for traj in parsed_trajs:    
             traj = np.array(traj)
-            for i in range(len(traj) - window):
+            for i in range(len(traj) - max_window):
                 prev_obs = traj[i][0]
                 act = traj[i][1]
                 obs = traj[i][2]
                 r = traj[i][3]
                 discounted_R = 0
+                #for window in range(max_window):
                 for idx, sub_trans in enumerate(traj[i:i + window]):
                     discounted_R += self.gamma**(idx) * sub_trans[3]
-                if value_dataset.value_type == 'v':
-                    inputs = th.FloatTensor(np.array([prev_obs]))
-                    sub_Q = value_dataset.sub_q_model.model(inputs).detach().numpy().flatten()[0]
-                    opt_Q = value_dataset.opt_q_model.model(inputs).detach().numpy().flatten()[0]
-                elif value_dataset.value_type == 'q':
-                    inputs = th.FloatTensor(np.array([np.hstack([prev_obs, act])]))
-                    sub_Q = value_dataset.sub_q_model.model(inputs).detach().numpy().flatten()[0]
-                    opt_Q = value_dataset.opt_q_model.model(inputs).detach().numpy().flatten()[0]
+
+                prev_nth_discounted_R = 0
+                prev_id = np.max([i - window, 0])
+                for idx, prev_sub_trans in enumerate(traj[prev_id:i]):
+                    prev_nth_discounted_R += self.gamma**(idx) * prev_sub_trans[3]
+                
                 self.expert_replay_buffer.add(prev_obs,
                                                obs,
                                                act,
@@ -1436,7 +1469,11 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                                traj[i + window][0],
                                                traj[i + window][1],
                                                False,
+                                               traj[prev_id][0],
+                                               traj[prev_id][1],
+                                               prev_nth_discounted_R,
                                                self.gamma**window)
+
                 self.replay_buffer.add(prev_obs,
                                        obs,
                                        act,
@@ -1454,6 +1491,12 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                                                traj[i + window][0],
                                                traj[i + window][1],
                                                False,
+                                               traj[prev_id][0],
+                                               traj[prev_id][1],
+                                               prev_nth_discounted_R,
                                                self.gamma**window)
+
+                self.state_action_buffer.add(prev_obs,act)
                 #print(discounted_R, r, i)
+
 

trainer.py
@@ -92,6 +92,7 @@ def make_env(env_id, rank, seed=0):
     return _init
 
 def generate_suboptimal_trajectories(environment, bc_model, rewarder, sa_classifier, n_trajs=15):
+    environment.render()
     obs = environment.reset()
     trajectories = []
     for ep in range(n_trajs):
@@ -107,8 +108,9 @@ def generate_suboptimal_trajectories(environment, bc_model, rewarder, sa_classif
             prev_obs = obs
             inputs = torch.FloatTensor(np.array([obs]))
             # action =
-            action = bc_model.model(inputs).detach().numpy()[0] + (np.random.random_sample((environment.action_space.shape[-1],)) - 0.5) * 1.3
-            # print(action)
+            action = bc_model.model(inputs).detach().numpy()[0]
+
+            action = action #+ (np.random.random_sample((environment.action_space.shape[-1],)) - 0.5) * 1.3
             obs, reward, done, info = environment.step(action)
             obs_act = {'observation': prev_obs, 'action': action}
             imitation_reward = rewarder.compute_reward(obs_act)
@@ -151,7 +153,8 @@ def init_datasets_and_models(demonstrations, environment, imitation_rewarder,
             pickle.dump(bc_dataset, output, pickle.HIGHEST_PROTOCOL)
 
     
-
+    print(bc_dataset.xs[0].shape, bc_dataset.ys[0].shape, )
+    print(environment.action_space.shape, environment.observation_space.shape)
     try:        
         with open(sa_classifier_pkl_pth, 'rb') as inputs:
                 sa_classifier = pickle.load(inputs)
@@ -182,7 +185,7 @@ def init_datasets_and_models(demonstrations, environment, imitation_rewarder,
             bc_model = pickle.load(inputs)
     except:
         bc_model = behavior_cloning.BehaviorCloning(train_loader=bc_dataset.train_loader, x_dim=bc_dataset.xs[0].shape[0],
-                                                y_dim=bc_dataset.ys[0].shape[0], epochs=200)
+                                                y_dim=bc_dataset.ys[0].shape[0], epochs=800)
         bc_model.train_BC()
         with open(bc_model_pkl_pth, 'wb') as output:
             pickle.dump(bc_model, output, pickle.HIGHEST_PROTOCOL)
@@ -259,8 +262,8 @@ def main(_):
     environment = gym.make(FLAGS.env_name)
     environment = TimeLimit(environment, max_episode_steps=1000)
     environment.seed(FLAGS.random_seed)
+    environment.action_space.seed(RANDOM_SEED)
     set_random_seed(FLAGS.random_seed)
-    
     # Create the vectorized environment
     #num_cpu =  1 # Number of processes to use
     #environment = VecNormalize(DummyVecEnv([lambda: gym.make(FLAGS.env_name)]), norm_obs=True, norm_reward=True,clip_obs=10.)
@@ -270,7 +273,7 @@ def main(_):
     #environment.action_space.seed(RANDOM_SEED)
 
     n_actions = environment.action_space.shape[-1]
-    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0. * np.ones(n_actions))
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.5 * np.ones(n_actions))
 
     
     bc_dataset, sa_classifier, bc_model, suboptimal_trajs, value_dataset = init_datasets_and_models(demonstrations,
@@ -282,10 +285,17 @@ def main(_):
                                                                                                     bc_model_pkl_pth=bc_model_pkl_pth,
                                                                                                     suboptimal_trajs_pkl_pth=suboptimal_trajs_pkl_pth,
                                                                                                     value_dataset_pkl_pth=value_dataset_pkl_pth)
-
+    """
+    bc_model = behavior_cloning.BehaviorCloning(train_loader=bc_dataset.train_loader, x_dim=bc_dataset.xs[0].shape[0],
+                                            y_dim=bc_dataset.ys[0].shape[0], epochs=3000)
+    bc_model.train_BC()
+    generate_suboptimal_trajectories(environment, bc_model, imitation_rewarder, sa_classifier,
+                                                            n_trajs=555)
+    """
     # start training
     # define cutomized td3
     model = TD3('MlpPolicy', environment, action_noise=action_noise, verbose=1,
+                policy_delay=3,
                 rewarder=imitation_rewarder,
                 reward_type='pwil',
                 sl_dataset=bc_dataset,
@@ -300,10 +310,10 @@ def main(_):
 
     parsed_trajs = value_dataset.parse_demonstrations(demonstrations)
    
-    #model.pretrain_actor_using_demo()
+    #model.pretrain_actor_using_demo(epochs=50)
     model.add_expert_trajs_to_buffer(parsed_trajs, value_dataset)
-    model.pretrain_critic_using_demo()
-    model.learn(total_timesteps=1e6)
+    #model.pretrain_critic_using_demo()
+    model.learn(total_timesteps=1e6, log_interval=1)
 
     print("Logger outputs after training:", logger.Logger.CURRENT.output_formats)
 